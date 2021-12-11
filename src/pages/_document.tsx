63a726525395f5658521121aa5fa8ee87e00eb98
evfjunior <evfjunior@protonmail.com>
2021-12-11 01:21:31 -0300

    chore: change google fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -22,8 +22,8 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
+            href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&family=Raleway:wght@400;700&display=swap"
             rel="stylesheet"
-            href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap"
           />
         </Head>
         <body>
