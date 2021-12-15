fa5c57994fe4cf626c1dee7c431484ecc3b7bb31
evfjunior <evfjunior@protonmail.com>
2021-12-15 01:50:16 -0300

    chore(storybook): reconfigure static directories due to deprecation warning

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,7 @@
 module.exports = {
   stories: ['../src/components/**/stories.tsx'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  staticDirs: ['../public'],
   core: { builder: 'webpack5' },
   webpackFinal: (config) => {
     config.resolve.modules.push(`${process.cwd()}/src`)
