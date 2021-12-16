8f0a3df2c9a41b8d740b931128ab279695f2e74c
evfjunior <evfjunior@protonmail.com>
2021-12-16 00:48:49 -0300

    chore(storybook): add dark mode

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,10 @@
 module.exports = {
   stories: ['../src/components/**/stories.tsx'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-links',
+    'storybook-dark-mode'
+  ],
   staticDirs: ['../public'],
   core: { builder: 'webpack5' },
   webpackFinal: (config) => {
