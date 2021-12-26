0492d0ea0896e4ab94a6580212163590acb89ef0
evfjunior <evfjunior@protonmail.com>
2021-12-25 22:15:13 -0300

    fix(logo): remake component to dinamically receive props via an api

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -3,16 +3,42 @@ import { render, screen } from '@testing-library/react'
 import { Logo } from '.'
 
 describe('<Logo />', () => {
-  it('renders the logo correctly', () => {
-    render(<Logo />)
-    const logo = screen.getByLabelText('fylo logo')
+  it('renders the medium size logo by default', () => {
+    const alt = 'Fylo logo'
+    const src = '/assets/logo.svg'
+
+    render(<Logo src={src} alt={alt} />)
+
+    const logo = screen.getByAltText(alt)
+    const link = screen.getByRole('link')
+
     expect(logo).toBeInTheDocument()
+    expect(link).toHaveStyle({ height: '2.2rem', width: '7rem' })
   })
 
-  it('links to the home page', () => {
-    render(<Logo />)
-    const logo = screen.getByLabelText('fylo logo')
+  it('renders a large size logo', () => {
+    const alt = 'Fylo logo'
+    const src = '/assets/logo.svg'
+
+    render(<Logo src={src} alt={alt} size="large" />)
+
+    const logo = screen.getByAltText(alt)
+    const link = screen.getByRole('link')
+
     expect(logo).toBeInTheDocument()
-    expect(logo).toHaveAttribute('href', '/')
+    expect(link).toHaveStyle({ height: '4.8rem', width: '16rem' })
+  })
+
+  it('links to the home page correctly', () => {
+    const alt = 'Fylo logo'
+    const src = '/assets/logo.svg'
+
+    render(<Logo src={src} alt={alt} size="large" />)
+
+    const logo = screen.getByAltText(alt)
+    const link = screen.getByRole('link')
+
+    expect(logo).toBeInTheDocument()
+    expect(link).toHaveAttribute('href', '/')
   })
 })
